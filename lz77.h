e9ee00881d3beeda77aec4b56cd67ebfb8ebbc96
GravisZro <noreply+github@adaptivetime.com>
2023-04-05 10:18:05 -0400

    input data is const

--- lz77.h
+++ lz77.h
@@ -6,8 +6,8 @@
 extern "C" {
 #endif
 
-uint32_t lz77_compress(uint8_t *input, uint32_t input_length, uint8_t *output, uint32_t output_limit);
-uint32_t lz77_decompress(uint8_t *input, uint32_t input_length, uint8_t *output, uint32_t output_limit);
+uint32_t lz77_compress(const uint8_t *input, uint32_t input_length, uint8_t *output, uint32_t output_limit);
+uint32_t lz77_decompress(const uint8_t *input, uint32_t input_length, uint8_t *output, uint32_t output_limit);
 
 #ifdef __cplusplus
 }
